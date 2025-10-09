b1bd5c7a2943f16897930e9f887d85498fa7aa90
Thiago Schulz <thiagoschulz1310@gmail.com>
2025-10-08 22:01:37 -0300

    Arrumando o erro visual do efeito Parallax

--- Assets/Script/parallax.cs
+++ Assets/Script/parallax.cs
@@ -21,7 +21,7 @@ public class parallax : MonoBehaviour
         float efeitoParallax = (cameraPosicao.x - camera.position.x) * qtdParallax; //PEGA O VALOR DA DISTANCIA MOVIMENTADA E APLICA O EFEITO
         float bgDestino = fundo.position.x + efeitoParallax;
 
-        Vector3 posicaoBg = new Vector3(bgDestino, fundo.position.y, fundo.position.x);
+        Vector3 posicaoBg = new Vector3(bgDestino, fundo.position.y, fundo.position.z);
         fundo.position = Vector3.Lerp(fundo.position, posicaoBg, velocidade * Time.deltaTime);    //Time.deltaTime = tempo que passou de um frame para outro
 
         cameraPosicao = camera.position;
